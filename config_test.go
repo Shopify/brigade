dfe70a6e9aa687271cc1d4d51f7b9fbd010b2d53
Tom Burns <tom.burns@jadedpixel.com>
2013-01-31 13:18:39 -0500

    test config load

--- config_test.go
+++ config_test.go
@@ -5,10 +5,44 @@ import "bytes"
 
 func TestTargetLoadFromJSON(t *testing.T) {
   jsonTarget := bytes.NewBufferString(`
-    {"Server": "s3.com", "BucketName": "storage_bucket", "AccessKey": "1234", "SecretAccessKey": "MySecretAccessKey"}
+    {
+      "Source": {"Server": "s3.com", "BucketName": "source_bucket", "AccessKey": "1234", "SecretAccessKey": "MySecretAccessKey"},
+      "Destination": {"Server": "storage.google.com", "BucketName": "google_bucket", "AccessKey": "abcd", "SecretAccessKey": "MyOtherSecretAccessKey"}
+    }
   `)
 
   loadConfig(jsonTarget)
 
-  t.Error("this is an error")
+  if env.Source.Server != "s3.com" {
+    t.Error("env.Source.Server incorrect")
+  }
+
+  if env.Source.BucketName != "source_bucket" {
+    t.Error("env.Source.BucketName incorrect")
+  }
+
+  if env.Source.AccessKey != "1234" {
+    t.Error("env.Source.AccessKey incorrect")
+  }
+
+  if env.Source.SecretAccessKey != "MySecretAccessKey" {
+    t.Error("env.Source.SecretAccessKey incorrect")
+  }
+
+  if env.Destination.Server != "storage.google.com" {
+    t.Error("env.Destination.Server incorrect")
+  }
+
+  if env.Destination.BucketName != "google_bucket" {
+    t.Error("env.Destination.BucketName incorrect")
+  }
+
+  if env.Destination.AccessKey != "abcd" {
+    t.Error("env.Destination.AccessKey incorrect")
+  }
+
+  if env.Destination.SecretAccessKey != "MyOtherSecretAccessKey" {
+    t.Error("env.Destination.SecretAccessKey incorrect")
+  }
+
 }
