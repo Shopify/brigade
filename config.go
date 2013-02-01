e0db8285e593902347c7e999eedf7ef961b436c0
Tom Burns <tom.burns@jadedpixel.com>
2013-01-31 20:27:53 -0500

    go fmt

--- config.go
+++ config.go
@@ -6,44 +6,44 @@ import "log"
 import "encoding/json"
 
 type Target struct {
-  Server string
-  BucketName string
-  AccessKey string
-  SecretAccessKey string
+	Server          string
+	BucketName      string
+	AccessKey       string
+	SecretAccessKey string
 }
 
 type Config struct {
-  Source Target
-  Destination Target
+	Source      Target
+	Destination Target
+	Workers     int
 }
 
 var env Config
 
 func readConfig() {
-  configFile := os.Getenv("ENV")
+	configFile := os.Getenv("ENV")
 
-  if configFile == "" {
-    configFile = "config/default.json"
-  }
+	if configFile == "" {
+		configFile = "config/default.json"
+	}
 
-  log.Printf("Loading environment from %s (override with ENV=xxx)", configFile)
+	log.Printf("Loading environment from %s (override with ENV=xxx)", configFile)
 
-  f, err := os.Open(configFile)
+	f, err := os.Open(configFile)
 
-  if err != nil {
-    log.Printf("Error opening config file: %s", err)
-    return
-  }
+	if err != nil {
+		log.Printf("Error opening config file: %s", err)
+		return
+	}
 
-  loadConfig(f)
+	loadConfig(f)
 }
 
 func loadConfig(source io.Reader) {
-  err := json.NewDecoder(source).Decode(&env)
+	err := json.NewDecoder(source).Decode(&env)
 
-  if err != nil {
-    log.Fatalf("Error parsing config file: %s", err)
-    return
-  }
+	if err != nil {
+		log.Fatalf("Error parsing config file: %s", err)
+		return
+	}
 }
-
