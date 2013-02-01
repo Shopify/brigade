8da563901f28e141f67822f9f0e93d5bf1797197
Tom Burns <tom.burns@jadedpixel.com>
2013-01-31 20:27:12 -0500

    For remote testing use environment variables for S3 connection

--- brigade_test.go
+++ brigade_test.go
@@ -1,6 +1,11 @@
 package brigade
 
+import "os"
 import "testing"
 
 func TestSanity(t *testing.T) {
+	if os.Getenv("ACCESS_KEY") == "" || os.Getenv("SECRET_ACCESS_KEY") == "" ||
+		os.Getenv("BUCKET") == "" || os.Getenv("AWS_HOST") == "" {
+		t.Error("Please set ACCESS_KEY, SECRET_ACCESS_KEY, BUCKET, and HOST variables for integration tests")
+	}
 }
