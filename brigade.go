ee6db787852412586130ca48710eb51648351ebb
Tom Burns <tom.burns@jadedpixel.com>
2013-01-31 20:28:50 -0500

    start scoping out actual copier

---
+++ brigade.go
@@ -0,0 +1,51 @@
+package brigade
+
+import (
+	"container/list"
+	"launchpad.net/goamz/aws"
+	"launchpad.net/goamz/s3"
+	"log"
+)
+
+var dirQueue *list.List
+var fileQueue chan string
+
+type S3Connection struct {
+	Connection *s3.S3
+	Target     *Target
+}
+
+func (s *S3Connection) Connect() {
+	auth := aws.Auth{s.Target.AccessKey, s.Target.SecretAccessKey}
+	s.Connection = s3.New(auth, aws.Region{S3Endpoint: s.Target.Server})
+
+	if s.Connection == nil {
+		log.Fatalf("Could not connect to S3 endpoint %s", s.Target.Server)
+	}
+}
+
+func (s *S3Connection) fileWorker() {
+	// pull files off channel, copy with permissions
+}
+
+func CopyBucket() {
+	dirQueue = list.New()
+
+	// spawn workers
+	for i := 0; i < env.Workers; i++ {
+		go fileCopier()
+	}
+
+}
+
+func copyDirectory(dir string) {
+	// get list on source and destination
+
+	// push subdirectories onto directory queue
+
+	// push changed files onto file queue
+
+}
+
+func fileCopier() {
+}
