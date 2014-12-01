2c2d072c08102f3e4a51b24496dbea6afe7c0699
Justin Li <jli.justinli@gmail.com>
2014-12-01 14:39:11 -0500

    Remove task_id to prevent the formatter from corrupting logrus

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"code.google.com/p/go-uuid/uuid"
-	"encoding/hex"
 	"github.com/Sirupsen/logrus"
-	"github.com/aybabtme/formatter"
 	"net/http"
 	"os"
 	"os/signal"
@@ -65,12 +62,6 @@ func main() {
 	go func() {
 		// don't monitor short tasks
 		time.Sleep(time.Second * 2)
-
-		id := hex.EncodeToString(uuid.NewUUID())[0:5]
-		// long tasks will have each field tagged with a task id
-		logrus.SetFormatter(formatter.Before(func(e *logrus.Entry) *logrus.Entry {
-			return e.WithField("task_id", id)
-		}, &logrus.TextFormatter{}))
 
 		logrus.WithFields(logrus.Fields{
 			"addr":    addr,
